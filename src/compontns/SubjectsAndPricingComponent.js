ed78789f82c8001c484ffcccb7d528e3588508da
ali-xwave <asghar.xwave@gmail.com>
2024-08-03 19:54:01 -0700

    add model

--- src/compontns/SubjectsAndPricingComponent.js
+++ src/compontns/SubjectsAndPricingComponent.js
@@ -5,11 +5,15 @@ import icon1 from "../Images/Left-icon.svg";
 import icon2 from "../Images/Right-icon.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import useAosInit from "./UseAosInit";
+// import "./SubjectsAndPricingComponent.css"; // Import custom CSS
 
 const SubjectsAndPricingComponent = () => {
+  useAosInit();
   const [selectedSubjects, setSelectedSubjects] = useState([]);
   const [newSubject, setNewSubject] = useState("");
-  const [showInput, setShowInput] = useState(false);
+  const [newSubjectLevel, setNewSubjectLevel] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
   const removeSubject = (subject) => {
     setSelectedSubjects(selectedSubjects.filter((item) => item !== subject));
@@ -41,17 +45,25 @@ const SubjectsAndPricingComponent = () => {
   };
 
   const handleAddNewSubject = () => {
-    if (newSubject && !selectedSubjects.includes(newSubject)) {
-      setSelectedSubjects([newSubject, ...selectedSubjects]);
+    if (
+      newSubject &&
+      newSubjectLevel &&
+      !selectedSubjects.some((subject) => subject.name === newSubject)
+    ) {
+      setSelectedSubjects([
+        { name: newSubject, level: newSubjectLevel },
+        ...selectedSubjects,
+      ]);
       setNewSubject(""); // Clear the input field after adding
-      setShowInput(false); // Hide the input field after adding
+      setNewSubjectLevel(""); // Clear the input field after adding
+      setShowModal(false); // Hide the modal after adding
     }
   };
 
   return (
     <div className="container-fluid">
       <div className="row row-Padding">
-        <div className="col-12 col-md-5 col-lg-5" data-aos="zoom-in-right">
+        <div className="col-12 col-md-5 col-lg-5" data-aos="fade-right">
           <img src={Man} alt="People" className="img-fluid" />
         </div>
         <div
@@ -63,39 +75,19 @@ const SubjectsAndPricingComponent = () => {
           <div className="column-bg rounded p-3">
             <div className="d-flex justify-content-between align-items-center mb-3">
               <h5>Select Subjects</h5>
-              <div>
-                {showInput ? (
-                  <div className="input-group">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Enter new subject"
-                      value={newSubject}
-                      onChange={(e) => setNewSubject(e.target.value)}
-                    />
-                    <button
-                      className="btn btn-outline-secondary"
-                      type="button"
-                      onClick={handleAddNewSubject}
-                    >
-                      Add
-                    </button>
-                  </div>
-                ) : (
-                  <button
-                    className="btn btn-outline-secondary"
-                    type="button"
-                    onClick={() => setShowInput(true)}
-                  >
-                    <FontAwesomeIcon icon={faPlus} />
-                  </button>
-                )}
-              </div>
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={() => setShowModal(true)}
+                style={{ position: "relative", zIndex: 1050 }}
+              >
+                <FontAwesomeIcon icon={faPlus} />
+              </button>
             </div>
             <div className="d-flex btns pb-2">
               {selectedSubjects.map((subject, index) => (
                 <button key={index}>
-                  {subject}{" "}
+                  {subject.name} - {subject.level}{" "}
                   <span onClick={() => removeSubject(subject)}>x</span>
                 </button>
               ))}
@@ -168,6 +160,70 @@ const SubjectsAndPricingComponent = () => {
           </div>
           <div className="Ellipse-style">
             <img src={Round} alt="People" />
+          </div>
+        </div>
+      </div>
+
+      {/* Modal */}
+      <div
+        className={`modal fade ${showModal ? "show" : ""}`}
+        style={{ display: showModal ? "block" : "none", zIndex: 1050 }}
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden={!showModal}
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content custom-modal">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Add Subject
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={() => setShowModal(false)}
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <div className="form-group">
+                <label htmlFor="newSubject">Select Subject</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="newSubject"
+                  value={newSubject}
+                  onChange={(e) => setNewSubject(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="newSubjectLevel">Select Subject Level</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="newSubjectLevel"
+                  value={newSubjectLevel}
+                  onChange={(e) => setNewSubjectLevel(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={() => setShowModal(false)}
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleAddNewSubject}
+              >
+                Add
+              </button>
+            </div>
           </div>
         </div>
       </div>
